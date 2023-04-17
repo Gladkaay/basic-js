55067bb9fd0430035332114c80c962d5fd606558
Nastya <nastya04052001@gmail.com>
2023-04-17 16:13:39 +0300

    feat: add task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let sep = options.separator;
+  if (sep == undefined) {
+    sep = "+";
+  }
+  let addSep = options.additionSeparator;
+  if (addSep == undefined) {
+    addSep = "|";
+  }
+  let repT = options.repeatTimes;
+  let addRepT = options.additionRepeatTimes;
+  let additionOpt = '', res = '';
+  if (repT == undefined) {
+    repT = 1;
+  }
+  if (addRepT == undefined) {
+    addRepT = 1;
+  }
+  if (str == null) {
+    str = 'null';
+  }
+  str = String(str);
+  if (String(options.addition) === 'null') {
+    options.addition = 'null';
+  }  
+  if (options.addition != undefined) {
+  opt = String(options.addition);
+  additionOpt = opt.concat('..').repeat(addRepT).slice(0,-2).replaceAll('..', addSep);
+  res = str.concat(additionOpt).concat('..').repeat(repT).slice(0,-2).replaceAll('..', sep)
+} else {
+  res = str.concat('..').repeat(repT).slice(0,-2).replaceAll('..', sep)
+}
+  return res;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    return deepCount(arr);
+    function deepCount(arr){
+      let result = arr.length;
+      for (let element of arr) {
+        if (Array.isArray(element)) {
+          result += deepCount(element);
+        }
+      };
+      return result;
+    }
   }
 }
 
